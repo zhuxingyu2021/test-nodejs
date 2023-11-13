45be6282330018f5c6ef8339b767512198178e80
zhuxingyu2021 <93047415+zhuxingyu2021@users.noreply.github.com>
2023-11-14 01:45:30 +0800

    a

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,12 +3,12 @@ import {Router} from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-    console.log("HELLO_WORLD 200\n")
+    console.log("HELLO_WORLD 200")
     res.json({message: "Hello world"})
 })
 
 router.get("/health", (req, res) => {
-    console.log("HEALTH 200\n")
+    console.log("HEALTH 200")
     res.status(200).json({message: "Everything is good here"})
 })
 
